738eb8b48df62a85785b564733d1a6f0c6febd2b
Lucquet Architectes <56298319+hyvenn@users.noreply.github.com>
2024-06-20 18:07:40 +0200

    Mack ONE visite virtuelle 3.2
    Nouvelle visite du projet Mack ONE

---
+++ data.js
@@ -0,0 +1,419 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vue-drone",
+      "name": "Vue drone",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.1233900545377837,
+        "pitch": 0.43623548194840467,
+        "fov": 1.4337299731675692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8799045928723483,
+          "pitch": 1.1304118863375017,
+          "rotation": 5.497787143782138,
+          "target": "4-schopf"
+        },
+        {
+          "yaw": 2.892129361770106,
+          "pitch": 0.864696746594042,
+          "rotation": 3.141592653589793,
+          "target": "5-ponton"
+        },
+        {
+          "yaw": 3.0406878829780197,
+          "pitch": 0.514631458378286,
+          "rotation": 3.141592653589793,
+          "target": "3-balcon"
+        },
+        {
+          "yaw": -0.2982336291315608,
+          "pitch": 0.7613591759668772,
+          "rotation": 3.141592653589793,
+          "target": "1-chemin-central"
+        },
+        {
+          "yaw": -0.058881419561348736,
+          "pitch": 0.37224264597055523,
+          "rotation": 3.141592653589793,
+          "target": "2-croisement"
+        },
+        {
+          "yaw": -2.5014620751099894,
+          "pitch": 0.40154677363393887,
+          "rotation": 3.141592653589793,
+          "target": "6-abris-vlo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-chemin-central",
+      "name": "chemin central",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.923689801189397,
+          "pitch": 0.011541472904243122,
+          "rotation": 1.5707963267948966,
+          "target": "4-schopf"
+        },
+        {
+          "yaw": -0.743299429719368,
+          "pitch": -0.0007282436489788324,
+          "rotation": 2.356194490192345,
+          "target": "2-croisement"
+        },
+        {
+          "yaw": 2.1201200722172313,
+          "pitch": 0.0033562370512356665,
+          "rotation": 3.141592653589793,
+          "target": "5-ponton"
+        },
+        {
+          "yaw": 2.092223545262634,
+          "pitch": -0.8202006126600168,
+          "rotation": 0,
+          "target": "0-vue-drone"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-croisement",
+      "name": "croisement",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33491332183553624,
+          "pitch": 0.009460928095965215,
+          "rotation": 9.42477796076938,
+          "target": "1-chemin-central"
+        },
+        {
+          "yaw": -0.1934015599582697,
+          "pitch": -0.004503953387477111,
+          "rotation": 7.853981633974483,
+          "target": "4-schopf"
+        },
+        {
+          "yaw": 0.17319063994042416,
+          "pitch": -0.5017926410595486,
+          "rotation": 0,
+          "target": "0-vue-drone"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-balcon",
+      "name": "balcon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.021061925726201025,
+          "pitch": 0.5500665218148235,
+          "rotation": 3.141592653589793,
+          "target": "5-ponton"
+        },
+        {
+          "yaw": 0.1734348764097362,
+          "pitch": 0.11075025126885052,
+          "rotation": 4.71238898038469,
+          "target": "4-schopf"
+        },
+        {
+          "yaw": -0.12454550704118894,
+          "pitch": -0.5317020373181087,
+          "rotation": 0,
+          "target": "0-vue-drone"
+        },
+        {
+          "yaw": -1.6059336339637085,
+          "pitch": -0.026401794469371964,
+          "rotation": 3.9269908169872414,
+          "target": "6-abris-vlo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-schopf",
+      "name": "Schopf",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1402454802748743,
+          "pitch": 0.024887392611494974,
+          "rotation": 4.71238898038469,
+          "target": "1-chemin-central"
+        },
+        {
+          "yaw": 1.435943374895852,
+          "pitch": -0.013982399916239174,
+          "rotation": 2.356194490192345,
+          "target": "5-ponton"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ponton",
+      "name": "ponton",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1239439470469517,
+          "pitch": -0.44785840847722014,
+          "rotation": 3.141592653589793,
+          "target": "3-balcon"
+        },
+        {
+          "yaw": -0.2856962650170196,
+          "pitch": -0.0238170435886893,
+          "rotation": 3.141592653589793,
+          "target": "1-chemin-central"
+        },
+        {
+          "yaw": 0.29502784464522414,
+          "pitch": -0.016570635083354546,
+          "rotation": 10.995574287564278,
+          "target": "4-schopf"
+        },
+        {
+          "yaw": -0.1452980809005915,
+          "pitch": -0.6269746622210537,
+          "rotation": 0,
+          "target": "0-vue-drone"
+        },
+        {
+          "yaw": -2.3125440110768523,
+          "pitch": -0.0294967000782691,
+          "rotation": 3.141592653589793,
+          "target": "6-abris-vlo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-abris-vlo",
+      "name": "abris vélo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06703111412793383,
+          "pitch": -0.33173864734075664,
+          "rotation": 6.283185307179586,
+          "target": "0-vue-drone"
+        },
+        {
+          "yaw": 0.5123062160580893,
+          "pitch": -0.01498418315242489,
+          "rotation": 3.141592653589793,
+          "target": "5-ponton"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mack ONE visite virtuelle 3",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
